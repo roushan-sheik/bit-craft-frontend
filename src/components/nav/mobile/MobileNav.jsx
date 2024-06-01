50a61c526c19285a7cdc13f37bccda2950f1a071
roushan-sheik <roushansheik@gmail.com>
2024-06-01 12:48:51 +0600

    nav bar updated

--- src/components/nav/mobile/MobileNav.jsx
+++ src/components/nav/mobile/MobileNav.jsx
@@ -2,22 +2,59 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const MobileNav = () => {
-    return (
-      <div>
-        <NavLink
-          to="/messages"
-          className={({ isActive, isPending, isTransitioning }) =>
-            [
-              isPending ? "pending" : "",
-              isActive ? "active" : "text_brand_sec font-bold",
-              isTransitioning ? "transitioning" : "",
-            ].join(" ")
-          }
-        >
-          Home
-        </NavLink>
-      </div>
-    );
+
+  return (
+    <div className="flex flex-col bg-gray-200 rounded-md  py-4 px-8 gap-2 absolute right-0 top-14">
+      <NavLink
+        to="/"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text_brand_sec font-bold" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/products"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text_brand_sec font-bold" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        Products
+      </NavLink>
+      <NavLink
+        to="/news"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text_brand_sec font-bold" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        News
+      </NavLink>
+      <NavLink
+        to="/community"
+        className={({ isActive, isPending, isTransitioning }) =>
+          [
+            isPending ? "pending" : "",
+            isActive ? "text_brand_sec font-bold" : "",
+            isTransitioning ? "transitioning" : "",
+          ].join(" ")
+        }
+      >
+        Community
+      </NavLink>
+    </div>
+  );
 };
 
 export default MobileNav;
--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,24 +1,48 @@
 import React from "react";
+import { HiMiniBars3BottomRight, HiMiniBarsArrowDown } from "react-icons/hi2";
+import { Link } from "react-router-dom";
 import DesktopNav from "../../nav/desktop/DesktopNav";
+import MobileNav from "../../nav/mobile/MobileNav";
 const Header = () => {
+  const [openBar, setOpenBar] = React.useState(false);
   return (
-    <div className="flex flex-grow-1 border py-3 lg:px-6 px-2">
+    <div className="flex justify-between  flex-grow-1 border shadow-sm py-3 lg:px-6 px-2">
       {/* left side main logo  */}
-      <div className="flex basis-[25%] mb-2 items-center relative">
-        <img className="lg:h-10 h-8" src="./bitcraft.jpeg" alt="BitCraft" />
-        <p className="lg:text-3xl text-2xl absolute lg:left-[2.10rem] left-[1.70rem] font-extrabold mt-2">
-          it<span className="lg:text-4xl text-3xl text_brand_sec">C</span>raft
-        </p>
-      </div>
+      <Link to={"/"}>
+        <div className="flex cursor-pointer lg:basis-[25%] mb-2 items-center relative">
+          <img className="lg:h-10 h-8" src="./bitcraft.jpeg" alt="BitCraft" />
+          <p className="lg:text-3xl text-2xl absolute lg:left-[2.10rem] left-[1.70rem] font-extrabold mt-2">
+            it<span className="lg:text-4xl text-3xl text_brand_sec">C</span>raft
+          </p>
+        </div>
+      </Link>
       {/*middle position routes  */}
-      <div className="basis-[50%] flex items-center">
+      <div className="lg:basis-[50%] flex items-center">
         {/* desktop nav  */}
         <div className="hidden lg:flex">
           <DesktopNav />
         </div>
+        {/* mobile nav list  */}
+        <div className="lg:hidden flex">
+          {openBar && <MobileNav />}
+
+          {openBar ? (
+            <HiMiniBarsArrowDown
+              className="text-4xl"
+              onClick={() => setOpenBar(!openBar)}
+            />
+          ) : (
+            <HiMiniBars3BottomRight
+              onClick={() => setOpenBar(!openBar)}
+              className="text-4xl"
+            />
+          )}
+        </div>
       </div>
       {/* right side profile */}
-      <div className="basis-[25%]"></div>
+      <div className="lg:flex hidden ">
+        
+      </div>
     </div>
   );
 };
