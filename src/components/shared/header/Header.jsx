597bc8c13d1c0e2e6e7b0b6b27d4ea1cb829483a
roushan-sheik <roushansheik@gmail.com>
2024-06-01 11:11:41 +0600

    modal added

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div>
+    {/* left side main logo  */ }
+    <div></div>
+    {/*middle position routes  */ }
+    <div></div>
+    {/* right side profile */ }
+    <div></div>
+  </div>;
 };
 
 export default Header;
