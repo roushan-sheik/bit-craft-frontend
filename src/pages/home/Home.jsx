c288337565e5c256f7ffc6026e3d92aa6f021d71
roushan-sheik <roushansheik@gmail.com>
2024-06-02 11:38:33 +0600

    featured product title added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,23 @@
-import { Button } from "@material-tailwind/react";
 import React from "react";
 import BannerSlider from "../../components/slider/BannerSlider";
+import Title from "../../components/title/Title";
 const Home = () => {
   return (
-    <div>
-      <div className="z-0">
+    <main>
+      {/* Banner section  */}
+      <section className="z-0">
         <BannerSlider />
-      </div>
-      <Button>Button</Button>
-    </div>
+      </section>
+      {/* Featured Section  */}
+      <section>
+        <Title
+          title={"Featured Products"}
+          description={
+            "Explore a platform where tech enthusiasts can discover, review, and share cutting-edge products like web apps, AI tools, software, games, and mobile apps. Stay ahead of the curve by finding the latest innovations in technology and connect with a community of like-minded individuals."
+          }
+        />
+      </section>
+    </main>
   );
 };
 
